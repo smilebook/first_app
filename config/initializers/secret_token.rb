9cf3907d9f7812bb5c026093189d2d945d995434
smilebook <sssocalled@gmail.com>
2012-12-16 01:27:41 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '105df596b3940e350557e28d116349cb6927869ac2a24f119d18541aa756b2d39ce7e1317e46b5a9f7e373bffbc56d341de2104e41bb2df7603e5c555f993742'
